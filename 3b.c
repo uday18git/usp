8e712b94cb9b0a1d1a150a5b7ce5880f2ca63187
Uday chowdary <102567732+uday18git@users.noreply.github.com>
2024-06-18 08:19:22 +0530

    Create 3b.c

---
+++ 3b.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<fcntl.h> // file control options
+#include<dirent.h> // directory entry
+#include<time.h> // time and date functions.
+#include<sys/stat.h> // data returned by the stat function
+
+int main()
+{
+        struct dirent *dir; // pointer to directory entry structure, this is a pointer because readdir returns a pointer to a dynamically managed directory entry.
+        struct stat mystat; // a structure to hold info about the file
+        DIR *dp; // a pointer to the directory stream
+        dp = opendir("."); // opens the current directory "." represents the current directory
+        if(dp)
+        {
+                while(dir = readdir(dp)){
+                        stat(dir->d_name,&mystat);  // inode number, file mode , uid of file owner, group id , time of last access , name of file or directiry
+                        printf("%ld %o %d %d %s %s\n",mystat.st_ino,mystat.st_mode,mystat.st_uid,mystat.st_gid,ctime(&mystat.st_atime),dir->d_name);
+                }
+        }
+}
+
+
+int main()
+{
+        struct stat mystat;
+        struct dirent *dir;
+        DIR *dp;
+        dp = opendir(".");
+        if(dp)
+        {
+                while(dir = readdir(dp)){
+                        stat(dir->dname,&mystat);
+                        printf("%ld %o %d %d %s\n",mystat.st_ino,mystat.st_mode,mystat.st_uid,mystat.st_gid,dir->dname);
+        }
+        }
+}
